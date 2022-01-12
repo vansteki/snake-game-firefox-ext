cd25d18771532fbef2a5b1455ff3a7573525088c
vansteki <vansteki.tw@gmail.com>
2022-01-12 17:50:28 +0800

    chore: track files

--- src/devtools/game/tailwind.config.js
+++ src/devtools/game/tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   content: [
     "src/devtools/game/index.html",
-    "src/devtools/game/app.js"
+    "src/devtools/game/app.js",
+    "src/devtools/game/base.css",
   ],
   theme: {
     extend: {},
