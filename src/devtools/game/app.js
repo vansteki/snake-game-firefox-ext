ef29943486c23e3abf9b030a4115d69161787ffe
vansteki <vansteki.tw@gmail.com>
2022-01-12 18:42:51 +0800

    style: reformat by npm run format

--- src/devtools/game/app.js
+++ src/devtools/game/app.js
@@ -6,16 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 10
   let currentIndex = 0 //so first div in our grid
   let appleIndex = 0 //so first div in our grid
-  let currentSnake = [2,1,0]
+  let currentSnake = [2, 1, 0]
   let direction = 1
   let score = 0
   let speed = 0.9
   let intervalTime = 0
   let interval = 0
 
-
   //to start, and restart the game
-  function startGame() {
+  function startGame () {
     currentSnake.forEach(index => squares[index].classList.remove('snake'))
     squares[appleIndex].classList.remove('apple')
     clearInterval(interval)
@@ -24,33 +23,31 @@ document.addEventListener('DOMContentLoaded', () => {
     direction = 1
     scoreDisplay.innerText = score
     intervalTime = 1000
-    currentSnake = [2,1,0]
+    currentSnake = [2, 1, 0]
     currentIndex = 0
     currentSnake.forEach(index => squares[index].classList.add('snake'))
     interval = setInterval(moveOutcomes, intervalTime)
   }
 
-
   //function that deals with ALL the ove outcomes of the Snake
-  function moveOutcomes() {
-
+  function moveOutcomes () {
     //deals with snake hitting border and snake hitting self
     if (
-      (currentSnake[0] + width >= (width * width) && direction === width ) || //if snake hits bottom
-      (currentSnake[0] % width === width -1 && direction === 1) || //if snake hits right wall
+      (currentSnake[0] + width >= width * width && direction === width) || //if snake hits bottom
+      (currentSnake[0] % width === width - 1 && direction === 1) || //if snake hits right wall
       (currentSnake[0] % width === 0 && direction === -1) || //if snake hits left wall
-      (currentSnake[0] - width < 0 && direction === -width) ||  //if snake hits the top
+      (currentSnake[0] - width < 0 && direction === -width) || //if snake hits the top
       squares[currentSnake[0] + direction].classList.contains('snake') //if snake goes into itself
     ) {
       return clearInterval(interval) //this will clear the interval if any of the above happen
     }
 
     const tail = currentSnake.pop() //removes last ite of the array and shows it
-    squares[tail].classList.remove('snake')  //removes class of snake from the TAIL
+    squares[tail].classList.remove('snake') //removes class of snake from the TAIL
     currentSnake.unshift(currentSnake[0] + direction) //gives direction to the head of the array
 
     //deals with snake getting apple
-    if(squares[currentSnake[0]].classList.contains('apple')) {
+    if (squares[currentSnake[0]].classList.contains('apple')) {
       squares[currentSnake[0]].classList.remove('apple')
       squares[tail].classList.add('snake')
       currentSnake.push(tail)
@@ -64,21 +61,19 @@ document.addEventListener('DOMContentLoaded', () => {
     squares[currentSnake[0]].classList.add('snake')
   }
 
-
   //generate new apple once apple is eaten
-  function randomApple() {
-    do{
+  function randomApple () {
+    do {
       appleIndex = Math.floor(Math.random() * squares.length)
-    } while(squares[appleIndex].classList.contains('snake')) //making sure apples dont appear on the snake
+    } while (squares[appleIndex].classList.contains('snake')) //making sure apples dont appear on the snake
     squares[appleIndex].classList.add('apple')
   }
 
-
   //assign functions to keycodes
-  function control(e) {
+  function control (e) {
     squares[currentIndex].classList.remove('snake')
 
-    if(e.keyCode === 39) {
+    if (e.keyCode === 39) {
       direction = 1 //if we press the right arrow on our keyboard, the snake will go right one
     } else if (e.keyCode === 38) {
       direction = -width // if we press the up arrow, the snake will go back ten divs, appearing to go up
--- src/devtools/game/tailwind.config.js
+++ src/devtools/game/tailwind.config.js
@@ -1,11 +1,7 @@
 module.exports = {
-  content: [
-    "src/devtools/game/index.html",
-    "src/devtools/game/app.js",
-    "src/devtools/game/base.css",
-  ],
+  content: ['src/devtools/game/index.html', 'src/devtools/game/app.js', 'src/devtools/game/base.css'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
