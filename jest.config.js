583354461d210bd6c0321e204c66c5b675341679
vansteki <vansteki.tw@gmail.com>
2022-01-15 21:40:00 +0800

    refactor: update jest config file
    - disable coverage
    - add skip coverage paths

--- jest.config.js
+++ jest.config.js
@@ -26,9 +26,14 @@ module.exports = {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/coverage/",
+    ".github/",
+    ".run/",
+    "/screenshots/",
+    "/src/icons/"
+  ],
 
   // Indicates which provider should be used to instrument code for coverage
   // coverageProvider: "babel",
